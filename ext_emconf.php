82bb8d557c62ea67df92a8bf75ff2fff820ba2ba
Steffen Keuper <steffen.keuper@web.de>
2018-10-02 17:46:21 +0200

    [TASK] increase TYPO3 depends version, bump version

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,10 +21,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.3',
+    'version' => '1.0.4',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.1.99',
+            'typo3' => '8.7.0-9.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
